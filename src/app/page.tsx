4e3e7de435396d5317ce2d4923647f7b844a8003
Mohan <chaudharymohan2012@gmail.com>
2024-02-06 00:03:16 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { SelectDemo } from "@/components/Select";
+import { Button, Checkbox } from "@nextui-org/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      {/* <div className="bg-red-400">
+        hello world
+      </div>
+      <Button color="primary" className="my-10">
+        Button
+      </Button>
+      <Checkbox defaultSelected className="my-10">Option</Checkbox>
+      <SelectDemo /> */}
+    </div>
+  );
+}
