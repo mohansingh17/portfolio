216b62f63505468151c810a6c0a1863783cf2e3f
Mohan <chaudharymohan2012@gmail.com>
2024-02-07 01:14:29 +0530

    image issue on vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["portfolio-snowy-five-31.vercel.app", "portfolio-snowy-five-31.vercel.app/", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app/"], // Replace with your S3 domain
+        domains: ["https://portfolio-mohan.vercel.app", "https://portfolio-mohan.vercel.app/", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app/", "portfolio-mohan.vercel.app", "portfolio-mohan.vercel.app/"], // Replace with your S3 domain
     },
     // images: {
     //     remotePatterns: [
