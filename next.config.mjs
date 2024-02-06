d4d26e97d6ae4832708e230f97d5bc72495a25a1
Mohan <chaudharymohan2012@gmail.com>
2024-02-07 00:42:02 +0530

    add dimain for image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ["https://portfolio-snowy-five-31.vercel.app"], // Replace with your S3 domain
+    },
+};
 
 export default nextConfig;
