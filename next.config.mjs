c16579c23c0d7041db250372d489423d84fb36b9
Mohan <chaudharymohan2012@gmail.com>
2024-02-07 00:56:52 +0530

    hero banner mobile responsive

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["portfolio-snowy-five-31.vercel.app"], // Replace with your S3 domain
+        domains: ["portfolio-snowy-five-31.vercel.app", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app"], // Replace with your S3 domain
     },
 };
 
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -6,9 +6,9 @@ const HeroBanner = () => {
     return (
         <div className='bg-black text-white h-screen relative' style={{ height: "calc(100vh - 64px)" }}>
             <Image src="/images/hero.jpg" alt='mohan' height={200} width={200} className='h-full w-auto absolute top-0 left-1/2 -translate-x-1/2' />
-            <div className='flex flex-col gap-5 justify-center h-full pl-32 relative'>
+            <div className='flex flex-col gap-5 justify-center h-full md:pl-32 pl-5 relative'>
                 {/* <h4 className='text-3xl'>Hello, my name is</h4> */}
-                <h3 className='text-6xl font-semibold'>I'm Mohan Singh</h3>
+                <h3 className='md:text-6xl text-4xl font-semibold'>I'm Mohan Singh</h3>
                 <div className='flex items-center gap-2.5 h-24'>
                     {/* <div className='text-4xl font-semibold'>And I'm a</div> */}
                     <TypeAnimation
@@ -21,7 +21,7 @@ const HeroBanner = () => {
                             1000,
                         ]}
                         speed={50}
-                        className='text-4xl text-[#EE9322] z-50 font-semibold border-2 border-[#EE9322] pt-3 pb-5 px-5 rounded-md'
+                        className='md:text-4xl text-3xl text-[#EE9322] z-10 font-semibold border-2 border-[#EE9322] pt-3 pb-5 px-5 rounded-md'
                         repeat={Infinity}
                     />
                 </div>
