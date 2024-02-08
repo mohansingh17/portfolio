42da5f083fad98fa3dc777aa627f092a67033137
Mohan <chaudharymohan2012@gmail.com>
2024-02-08 20:22:48 +0530

    image issue testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["https://portfolio-mohan.vercel.app", "https://portfolio-mohan.vercel.app/", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app/", "portfolio-mohan.vercel.app", "portfolio-mohan.vercel.app/"], // Replace with your S3 domain
-    },
+    // images: {
+    //     domains: ["https://portfolio-mohan.vercel.app", "https://portfolio-mohan.vercel.app/", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app", "portfolio-enh3y2bny-chaudharymohan2012-gmailcom.vercel.app/", "portfolio-mohan.vercel.app", "portfolio-mohan.vercel.app/"], // Replace with your S3 domain
+    // },
     // images: {
     //     remotePatterns: [
     //         {
