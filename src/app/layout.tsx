7a789e44baded95334c7b16c152c7e40c53eeaba
Mohan <chaudharymohan2012@gmail.com>
2025-11-01 00:56:27 +0530

    console user agent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,6 +61,7 @@ export default function RootLayout({
         {!isMobileOrTablet && <ParticleBackground />}
         {children}
         <Contact />
+        {userAgent}
       </body>
     </html>
   );
